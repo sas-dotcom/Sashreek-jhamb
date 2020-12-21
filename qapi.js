9ea82daf72eb4137356463a974a76bae249be993
sas-dotcom <76430191+sas-dotcom@users.noreply.github.com>
2020-12-21 15:44:19 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fe06e6398cea1001eae279b",
+				"5fe06e6398cea1001eae279d"
+			]
+		},
+		"5fe06e6398cea1001eae279b": {
+			"id": "5fe06e6398cea1001eae279b",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fe06e6398cea1001eae279d": {
+			"id": "5fe06e6398cea1001eae279d",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,230 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Icon, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, StackItem, Stack, Section } from "@quarkly/components";
+import { MdArrowDownward } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1510125594188-5afc74c8cc43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+				</StackItem>
+			</Stack>
+			<Text color="--lightD2" letter-spacing="1px" text-transform="uppercase" margin="0">
+				I provide PHOTOS and videos the best cars ever
+				<br />
+			</Text>
+			<Box text-align="center" margin="96px 0 0 0">
+				<Text margin="8px 0" text-transform="uppercase">
+					Who we are
+				</Text>
+				<Icon category="md" margin="0 auto" icon={MdArrowDownward} />
+			</Box>
+		</Section>
+		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						color="--dark"
+						padding="98px 64px"
+						background="--color-light"
+						flex-direction="column"
+					/>
+					<Text
+						as="h4"
+						margin="12px 0"
+						font="--base"
+						color="--grey"
+						letter-spacing="1px"
+						text-transform="uppercase"
+					>
+						Sashreek Jhamb
+					</Text>
+					<Text as="h2" margin="12px 0" font="--headline2" md-font="--headline3">
+						Best car images website
+						<br />
+					</Text>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Stack>
+						<StackItem width="100%" lg-width="33.3%" md-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1575999080555-3f7a698dd8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1577315734214-4b3dec92d9ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1575999502951-4ab25b5ca889?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1568&q=80) center/cover" />
+						</StackItem>
+					</Stack>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://uploads.quarkly.io/5fe06e6398cea1001eae2798/images/Mercedes-Benz-Silver-Arrow-Concept-05-720x540.jpg?v=2020-12-21T09:57:50.585Z) center/cover">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text font="--base">
+					The Mercedes Benz silver lightning
+				</Text>
+			</Box>
+		</Section>
+		<Section
+			padding="140px 0"
+			sm-padding="40px 0"
+			background="linear-gradient(0deg,rgba(0,0,0,.2) 0%,rgba(0,0,0,.2) 100%),--color-dark url(https://uploads.quarkly.io/5fe06e6398cea1001eae2798/images/bugatti-chiron-pur-sport.jpg?v=2020-12-21T09:59:30.223Z) 50% 15%/cover"
+			color="--light"
+			font="--base"
+		>
+			<Box>
+				<Text
+					as="h1"
+					margin="0 0 96px 0"
+					text-align="center"
+					letter-spacing="50px"
+					text-transform="uppercase"
+					font="100 132px/1.2 --fontFamily-sans"
+					lg-font="200 42px/1.2 --fontFamily-sans"
+					lg-letter-spacing="20px"
+				>
+					Buggati
+					<br />
+					Chiron
+					<br />
+				</Text>
+			</Box>
+			<Stack>
+				<StackItem
+					width="25%"
+					lg-width="50%"
+					sm-width="100%"
+					font="--lead"
+					text-transform="uppercase"
+					letter-spacing="1px"
+				/>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" />
+				<StackItem width="25%" lg-width="50%" sm-width="100%" />
+			</Stack>
+		</Section>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://uploads.quarkly.io/5fe06e6398cea1001eae2798/images/general.webp?v=2020-12-21T10:04:21.716Z) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text color="--lightD2" letter-spacing="1px" text-transform="uppercase" margin="0">
+						Excellence in everything
+					</Text>
+					<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0">
+						RENAULT EZ-TOURNEE CONCEPT CAR Read more at:{" "}
+					</Text>
+				</StackItem>
+			</Stack>
+			<Box text-align="center" margin="96px 0 0 0" />
+		</Section>
+		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						margin-right="-20%"
+						padding-bottom="120%"
+						background="url(https://uploads.quarkly.io/5fe06e6398cea1001eae2798/images/Screenshot%202020-12-21%20at%203.37.14%20PM.png?v=2020-12-21T10:07:28.312Z) 50% 15%/cover"
+						lg-margin-right="0"
+						lg-padding-bottom="80%"
+					/>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						padding="98px 64px"
+						margin-left="-20%"
+						margin-top="36px"
+						background="--color-red"
+						color="--light"
+						mix-blend-mode="lighten"
+						lg-margin-left="0"
+						lg-margin-top="0"
+						flex-direction="column"
+					/>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section
+			padding="140px 0"
+			sm-padding="40px 0"
+			background="linear-gradient(0deg,rgba(0,0,0,.2) 0%,rgba(0,0,0,.2) 100%),--color-dark url(https://uploads.quarkly.io/5fe06e6398cea1001eae2798/images/9f279293-ds-x-concept-.webp?v=2020-12-21T10:10:21.780Z) 50% 15%/cover"
+			color="--light"
+			font="--base"
+		>
+			<Box>
+				<Text
+					as="h1"
+					margin="0 0 96px 0"
+					text-align="center"
+					letter-spacing="50px"
+					text-transform="uppercase"
+					font="100 132px/1.2 --fontFamily-sans"
+					lg-font="200 42px/1.2 --fontFamily-sans"
+					lg-letter-spacing="20px"
+				>
+					{" "}DS-X{"  "}
+				</Text>
+			</Box>
+			<Stack>
+				<StackItem
+					width="25%"
+					lg-width="50%"
+					sm-width="100%"
+					font="--lead"
+					text-transform="uppercase"
+					letter-spacing="1px"
+				/>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" />
+				<StackItem width="25%" lg-width="50%" sm-width="100%" />
+				<StackItem width="25%" lg-width="50%" sm-width="100%" />
+			</Stack>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
